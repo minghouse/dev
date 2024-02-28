813aae7974e15ea15affc26c42357d81695cbff5
daz-js <160233161+daz-js@users.noreply.github.com>
2024-02-28 14:28:11 +0000

    array

--- array.mjs
+++ array.mjs
@@ -31,6 +31,43 @@ const arr4 = arr3.find((v)=> {
     
     console.log(v.name=='ming') //還是給你參考好了
     return v.name == 'alex'
-    
+    //這個是指name和age都要符合條件
+    return v.name == 'alex' && v.age == 27 
+
+    //這個因為知道v是一個object，所以==右邊也給一個object，就會相同了
+    return v == {name:"alex", age: 27}
 })
 console.log(arr4)
+
+const students = [
+    { name: 'Alice', score: 85 },
+    { name: 'Bob', score: 92 },
+    { name: 'Charlie', score: 88 },
+    { name: 'David', score: 95 },
+  ];
+  
+  // 使用 Array.prototype.find() 找出分數超過 90 分的第一個學生
+  const highScorer = students.find((student) => {
+    return student.score > 90;
+  });
+  console.log(highScorer)
+
+//使用array.find()找到 >2的數字
+const array = [1, 2, 3, 4, 5];
+const array2 = array.find((number) => {
+    return number > 2;
+});
+console.log(array2)
+
+//使用.find()找到Bob的資料
+const people = [
+    { name: 'Alice', age: 22 },
+    { name: 'Bob', age: 28 },
+    { name: 'Charlie', age: 25 },
+    { name: 'David', age: 30 }
+  ];
+  const people2 = people.find((person) => {
+    return person.name == "Bob" ;
+});
+console.log(people2)
+
