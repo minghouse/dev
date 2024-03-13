909fbe0ce99fe12a73172a8d7c4ddf55a947ad4b
daz-js <160233161+daz-js@users.noreply.github.com>
2024-03-13 06:08:17 +0000

    try

--- try.mjs
+++ try.mjs
@@ -1,11 +1,14 @@
-const tpex_stock = async() =>{
-    const response = await fetch(`https://www.tpex.org.tw/web/stock/aftertrading/otc_quotes_no1430/stk_wn1430_result.php?l=zh-tw&d=103/03/11&se=EW`);
+
+const tpex_stock = async(date) =>{
+    const response = await fetch(`https://www.tpex.org.tw/web/stock/aftertrading/otc_quotes_no1430/stk_wn1430_result.php?date=${date}&type=24&response=json`);
     const data = await response.json();
     return data
 }
- const tpex = await tpex_stock()
+ const tpex = await tpex_stock("20240311")
+ 
  console.log("股票代號  名稱  股價   漲跌   成交金額")
- for (const v of tpex.data){
+ 
+ for (const v of tpex.aaData){
 
  console.log(`${v[0]} ${v[1]} ${v[2]} ${v[3]} ${v[8]}`)
  }
