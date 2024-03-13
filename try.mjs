5aaf42742955db5a71a3188c43b1ccb0a7442c7a
daz-js <160233161+daz-js@users.noreply.github.com>
2024-03-13 14:08:05 +0000

    TRY

--- try.mjs
+++ try.mjs
@@ -1,15 +1,29 @@
 
 const tpex_stock = async(date) =>{
-    const response = await fetch(`https://www.tpex.org.tw/web/stock/aftertrading/otc_quotes_no1430/stk_wn1430_result.php?date=${date}&type=24&response=json`);
+    //從這邊下手轉換date,把輸入的20240313轉成103/03/13
+    const date2 = formatDate(date) 
+    console.log(date2)
+    const response = await fetch(`https://www.tpex.org.tw/web/stock/aftertrading/otc_quotes_no1430/stk_wn1430_result.php?l=zh-tw&d=${encodeURIComponent(date2)}&se=EW`);
     const data = await response.json();
     return data
 }
- const date = "103/03/12"
+const formatDate = (date) => {
+    const year = date.substring(0, 4);
+    const month = date.substring(4, 6);
+    const day = date.substring(6, 8);
+
+    // 將年份轉換為兩位數
+    const shortYear = String(Number(year) - 1911);
+
+    return `${shortYear}/${month}/${day}`;
+}
+ const date = '20240313'
+ 
  const tpex = await tpex_stock(date)
 
  console.log("股票代號  名稱  股價   漲跌   成交金額")
  
  for (const v of tpex.aaData){
  
- console.log(`${v[0]} ${v[1]} ${v[2]} ${v[3]} ${v[8]}`)
+//  console.log(`${v[0]} ${v[1]} ${v[2]} ${v[3]} ${v[8]}`)
  }
