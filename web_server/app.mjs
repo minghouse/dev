9e42cd8e95954605768392edbe694272610ba978
ming.li <ming.li@iwplay.com.tw>
2024-05-24 10:45:55 +0800

    新增每日成交值的api

--- web_server/app.mjs
+++ web_server/app.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import google_sheet_search from './tasks/google/sheet_search.mjs';
 import google_sheet_insert from './tasks/google/sheet_insert.mjs';
 import getDatas from './tasks/google/getDatas.mjs';
+import getDatas2 from './tasks/google/getDatas2.mjs';
 
 import bodyParser from 'body-parser';
 
@@ -23,6 +24,9 @@ app.post('/google/sheet_insert', (req, res) => {
 });
 app.get('/google/getDatas', (req, res) => {
     getDatas(req, res)
+});
+app.get('/google/getDatas2', (req, res) => {
+    getDatas2(req, res)
 });
 
 const port = process.env.PORT || 3000;
