a79937ffe51a0bcac796f9aa169ebf94673ad45a
ming <ming@cccc>
2024-05-25 00:30:26 +0800

    1

--- web_server/client/getDatas.js
+++ web_server/client/getDatas.js
@@ -42,7 +42,7 @@ const getDatas = async () => {
     const sheetData_promise = []
     for (const v of sheetData_name) {
         const ai_start = ai_sn[v] - 1000 < 2 ? 2 : ai_sn[v] - 1000
-        sheetData_promise.push(common.accessGoogleSheets(SPREADSHEET_ID, `${v}!A${ai_start}:C${ai_sn[v]}`), accessToken)
+        sheetData_promise.push(common.accessGoogleSheets(SPREADSHEET_ID, `${v}!A${ai_start}:C${ai_sn[v]}`, accessToken))
     }
     const sheetData = await Promise.all(sheetData_promise)
     const values = sheetData.reduce((acc, cur) => acc.concat(cur.values), [])
