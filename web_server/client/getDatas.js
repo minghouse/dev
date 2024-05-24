05245565bb08a6c5b03c2e58c647f2324eff16db
ming <ming@cccc>
2024-05-24 23:59:58 +0800

    1

--- web_server/client/getDatas.js
+++ web_server/client/getDatas.js
@@ -3,5 +3,5 @@ import common from './modules.js';
 const SPREADSHEET_ID = '1DYU3NZmGLrj0G2ruQOyLxhOqLgBkSQ_mQ4-KPlYG-yE';
 const RANGE = '工作表8!A2:C5'
 
-const sheetData = await accessGoogleSheets(SPREADSHEET_ID, RANGE)
+const sheetData = await common.accessGoogleSheets(SPREADSHEET_ID, RANGE)
 console.log(sheetData)
