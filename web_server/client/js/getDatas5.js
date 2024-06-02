b7e093628f0cf1766c7dcab6097b3601351aae01
ming <ming@cccc>
2024-06-02 15:49:18 +0800

    1

--- web_server/client/js/getDatas5.js
+++ web_server/client/js/getDatas5.js
@@ -43,13 +43,211 @@ const getDatas5 = async (search_date) => {
     const [ai_sn_data, turnover_data, aftertrading_datas] = await Promise.all([
         common.accessGoogleSheets(SPREADSHEET_ID, 'AI整理-索引!A1:B20', accessToken),
         common.accessGoogleSheets(SPREADSHEET_ID, '自選股!A2:G1000', accessToken),
-        common.accessGoogleSheets(SPREADSHEET_ID, '每日收盤價!A2:B1000', accessToken)
+        // common.accessGoogleSheets(SPREADSHEET_ID, '每日收盤價!A2:B100', accessToken)
     ])
     
     //----------------------------------------------------
     //自選股整理
     const turnover_data_all = turnover_data.values
-    const aftertrading_data_all = aftertrading_datas.values
+    // const aftertrading_data_all = aftertrading_datas.values
+    
+    //順便去除重複
+    const result_20_name = turnover_data_all.map(v=>v[2]).filter((v, i, s)=>s.indexOf(v)===i) //股票名稱
+
+    //----------------------------------------------------
+
+    //上面註解的getDatas方法是appscript，這裡改成nodejs
+    const ai_sn = {}
+    for (const v of ai_sn_data.values) {
+        if (v[0]) {
+            ai_sn[v[0]] = v[1]
+        }
+    }
+
+    let sheetData_name = []
+    const sheetData = await (async ()=>{
+        try {
+            sheetData_name = [
+                "news",
+                "AI整理-經濟日報",
+                "AI整理-中國時報",
+                "AI整理-yahoo財經",
+                "AI整理-工商時報",
+                "AI整理-時報新聞",
+                "AI每周整理",
+                "每日收盤價",
+            ]
+            const sheetData_promise = []
+
+            for (const v of sheetData_name) {
+                const ai_start = (()=>{
+                    if (v == 'AI每周整理') {
+                        return ai_sn[v] - 20 < 2 ? 2 : ai_sn[v] - 20
+                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
+                        return ai_sn[v] - 1000 < 2 ? 2 : ai_sn[v] - 1000
+                    } else if (v == '每日收盤價') {
+                        return 2
+                    }
+                    return 2
+                })()
+                const ai_end = (()=>{
+                    if (v == 'AI每周整理') {
+                        return 20
+                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
+                        return ai_sn[v]
+                    } else if (v == '每日收盤價') {
+                        return 1000
+                    }
+                    return 4000
+                })()
+                
+                if (v == 'news') {
+                    sheetData_promise.push(new Promise(async (resolve, reject) => {
+                        try {
+                            const form_data = {
+                                select: '*',
+                                from: 'news',
+                                where: `where create_date > '${date_start.split(' ')[0]}' AND content REGEXP '(${result_20_name.join('|')})'`
+                            }
+                            const res = await fetch('http://127.0.0.1:3000/azure_mysql/select', {
+                            // const res = await fetch('https://dev-cpzu.onrender.com/azure_mysql/select', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify(form_data)
+                            })
+                            const data = await res.json()
+                            resolve(data)
+                        } catch (error) {
+                            reject(error)
+                        }
+                    }))
+                } else {
+                    sheetData_promise.push(common.accessGoogleSheets(SPREADSHEET_ID, `${v}!A${ai_start}:G${ai_end}`, accessToken))
+                }
+            }
+            return await Promise.all(sheetData_promise)
+        } catch (e) {
+            sheetData_name = [
+                "經濟日報",
+                "中國時報",
+                "yahoo財經",
+                "工商時報",
+                "時報新聞",
+                "AI整理-經濟日報",
+                "AI整理-中國時報",
+                "AI整理-yahoo財經",
+                "AI整理-工商時報",
+                "AI整理-時報新聞",
+                "AI每周整理",
+                "每日收盤價",
+            ]
+            const sheetData_promise = []
+            for (const v of sheetData_name) {
+                const ai_start = (()=>{
+                    if (v == 'AI每周整理') {
+                        return ai_sn[v] - 20 < 2 ? 2 : ai_sn[v] - 20
+                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
+                        return ai_sn[v] - 1000 < 2 ? 2 : ai_sn[v] - 1000
+                    } else if (v == '每日收盤價') {
+                        return 2
+                    }
+                    return 2
+                })()
+                const ai_end = (()=>{
+                    if (v == 'AI每周整理') {
+                        return 20
+                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
+                        return ai_sn[v]
+                    } else if (v == '每日收盤價') {
+                        return 1000
+                    }
+                    return 4000
+                })()
+                
+                sheetData_promise.push(common.accessGoogleSheets(SPREADSHEET_ID, `${v}!A${ai_start}:G${ai_end}`, accessToken))
+            }
+            return await Promise.all(sheetData_promise)
+        }
+    })()
+
+    //資料歸類
+    const values = {} //經濟日報, 中國時報, yahoo財經, 工商時報, 時報新聞
+    const getDatas5_datas = [] //AI每周整理
+    const values2_datas_split = [] //AI整理-經濟日報, AI整理-中國時報, AI整理-yahoo財經, AI整理-工商時報, AI整理-時報新聞
+    // const turnover_data_all = [] //每日漲幅排名
+    const aftertrading_data_all = [] //每日收盤價
+    for (const k in sheetData) {
+        if (sheetData_name[k] == 'AI每周整理') {
+            const getDatas5 = sheetData[k]
+            for (const v of getDatas5.values) {
+                if (v[0] < date_start) {
+                    continue
+                }
+                const news = JSON.parse(v[1])
+                for (const v2 of news) {
+                    const content = `${v2[1]}（${v2[0]}）：\n${v2[2]}\n`
+                    getDatas5_datas.push([v[0], 'AI每周整理', content])
+                }
+            }
+        } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(sheetData_name[k])) {
+            const values2_datas = sheetData[k]
+            for (const v of values2_datas.values) {
+                if (v[0] < date_start) {
+                    continue
+                }
+                const news = v[2].replace(/\n\n新聞出處：/,'\n新聞出處：').split("\n\n")
+                for (const v2 of news) {
+                    const v3 = v2.split("\n")
+                    const check = v3.filter(v4=>/^[0-9]+\./.test(v4)).length
+                    if (/：$/.test(v3[0]) && /^新聞出處/.test(v3[v3.length - 1]) && check == v3.length - 2 && v3.length > 2) {
+                        values2_datas_split.push([v[0], v[1], v2])
+                    }
+                }
+            }
+        } else if (sheetData_name[k] == '每日收盤價') {
+            aftertrading_data_all.push(...sheetData[k].values)
+        } else if (sheetData_name[k] == 'news') {
+            // CREATE TABLE `news` (
+            //     `id` BIGINT(19) NOT NULL AUTO_INCREMENT,
+            //     `source` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '新聞來源' COLLATE 'utf8mb4_general_ci',
+            //     `category` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '分類' COLLATE 'utf8mb4_general_ci',
+            //     `create_date` DATE NOT NULL COMMENT '建立日期',
+            //     `create_time` TIME NOT NULL COMMENT '建立時間',
+            //     `url` VARCHAR(700) NOT NULL DEFAULT '' COMMENT '網址' COLLATE 'utf8mb4_general_ci',
+            //     `title` TEXT NOT NULL COMMENT '標題' COLLATE 'utf8mb4_general_ci',
+            //     `short_content` TEXT NOT NULL COMMENT '摘要' COLLATE 'utf8mb4_general_ci',
+            //     `content` LONGTEXT NOT NULL COMMENT '內容' COLLATE 'utf8mb4_general_ci',
+            //     PRIMARY KEY (`id`) USING BTREE,
+            //     UNIQUE INDEX `source_url` (`source`, `url`) USING BTREE,
+            //     INDEX `source` (`source`) USING BTREE,
+            //     INDEX `create_date` (`create_date`) USING BTREE
+            // )
+            // COLLATE='utf8mb4_general_ci'
+            // ENGINE=InnoDB
+            // AUTO_INCREMENT=106
+            // ;
+            
+            const news = sheetData[k]
+            for (const v of news) {
+                if (!values[v.source]) {
+                    values[v.source] = []
+                }
+                values[v.source].push([v.category, `${dayjs(v.create_date).format('YYYY-MM-DD')} ${v.create_time.replace(/:[0-9]$/, '')}`, v.url, v.title, v.short_content, v.content])
+            }
+        } else {
+            values[sheetData_name[k]] = sheetData[k].values
+        }
+    }
+    
+    //排序values2_datas_split
+    values2_datas_split.sort(function (a, b) {
+        return a[0] < b[0] ? 1 : -1
+    })
+
+    //----------------------------------------------------
+    //每日收盤價整理
     let aftertrading_data = []
     let a_i = 0
     while (1) {
@@ -123,196 +321,6 @@ const getDatas5 = async (search_date) => {
             result3.push(v)
         }
     }
-
-    const result_20_name = result3.map(v=>v[1]) //股票名稱
-
-    //----------------------------------------------------
-
-    //上面註解的getDatas方法是appscript，這裡改成nodejs
-    const ai_sn = {}
-    for (const v of ai_sn_data.values) {
-        if (v[0]) {
-            ai_sn[v[0]] = v[1]
-        }
-    }
-
-    let sheetData_name = []
-    const sheetData = await (async ()=>{
-        try {
-            sheetData_name = [
-                "news",
-                "AI整理-經濟日報",
-                "AI整理-中國時報",
-                "AI整理-yahoo財經",
-                "AI整理-工商時報",
-                "AI整理-時報新聞",
-                "AI每周整理",
-            ]
-            const sheetData_promise = []
-
-            for (const v of sheetData_name) {
-                const ai_start = (()=>{
-                    if (v == 'AI每周整理') {
-                        return ai_sn[v] - 20 < 2 ? 2 : ai_sn[v] - 20
-                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
-                        return ai_sn[v] - 1000 < 2 ? 2 : ai_sn[v] - 1000
-                    } else if (v == '每日收盤價') {
-                        return 2
-                    }
-                    return 2
-                })()
-                const ai_end = (()=>{
-                    if (v == 'AI每周整理') {
-                        return 20
-                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
-                        return ai_sn[v]
-                    } else if (v == '每日收盤價') {
-                        return 1000
-                    }
-                    return 4000
-                })()
-                
-                if (v == 'news') {
-                    sheetData_promise.push(new Promise(async (resolve, reject) => {
-                        try {
-                            const form_data = {
-                                select: '*',
-                                from: 'news',
-                                where: `where create_date > '${date_start.split(' ')[0]}' AND content REGEXP '(${result_20_name.join('|')})'`
-                            }
-                            // const res = await fetch('http://127.0.0.1:3000/azure_mysql/select', {
-                            const res = await fetch('https://dev-cpzu.onrender.com/azure_mysql/select', {
-                                method: 'POST',
-                                headers: {
-                                    'Content-Type': 'application/json'
-                                },
-                                body: JSON.stringify(form_data)
-                            })
-                            const data = await res.json()
-                            resolve(data)
-                        } catch (error) {
-                            reject(error)
-                        }
-                    }))
-                } else {
-                    sheetData_promise.push(common.accessGoogleSheets(SPREADSHEET_ID, `${v}!A${ai_start}:G${ai_end}`, accessToken))
-                }
-            }
-            return await Promise.all(sheetData_promise)
-        } catch (e) {
-            sheetData_name = [
-                "經濟日報",
-                "中國時報",
-                "yahoo財經",
-                "工商時報",
-                "時報新聞",
-                "AI整理-經濟日報",
-                "AI整理-中國時報",
-                "AI整理-yahoo財經",
-                "AI整理-工商時報",
-                "AI整理-時報新聞",
-                "AI每周整理",
-            ]
-            const sheetData_promise = []
-            for (const v of sheetData_name) {
-                const ai_start = (()=>{
-                    if (v == 'AI每周整理') {
-                        return ai_sn[v] - 20 < 2 ? 2 : ai_sn[v] - 20
-                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
-                        return ai_sn[v] - 1000 < 2 ? 2 : ai_sn[v] - 1000
-                    } else if (v == '每日收盤價') {
-                        return 2
-                    }
-                    return 2
-                })()
-                const ai_end = (()=>{
-                    if (v == 'AI每周整理') {
-                        return 20
-                    } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(v)) {
-                        return ai_sn[v]
-                    } else if (v == '每日收盤價') {
-                        return 1000
-                    }
-                    return 4000
-                })()
-                
-                sheetData_promise.push(common.accessGoogleSheets(SPREADSHEET_ID, `${v}!A${ai_start}:G${ai_end}`, accessToken))
-            }
-            return await Promise.all(sheetData_promise)
-        }
-    })()
-
-    //資料歸類
-    const values = {} //經濟日報, 中國時報, yahoo財經, 工商時報, 時報新聞
-    const getDatas5_datas = [] //AI每周整理
-    const values2_datas_split = [] //AI整理-經濟日報, AI整理-中國時報, AI整理-yahoo財經, AI整理-工商時報, AI整理-時報新聞
-    // const turnover_data_all = [] //每日漲幅排名
-    // const aftertrading_data_all = [] //每日收盤價
-    for (const k in sheetData) {
-        if (sheetData_name[k] == 'AI每周整理') {
-            const getDatas5 = sheetData[k]
-            for (const v of getDatas5.values) {
-                if (v[0] < date_start) {
-                    continue
-                }
-                const news = JSON.parse(v[1])
-                for (const v2 of news) {
-                    const content = `${v2[1]}（${v2[0]}）：\n${v2[2]}\n`
-                    getDatas5_datas.push([v[0], 'AI每周整理', content])
-                }
-            }
-        } else if ([ "AI整理-經濟日報", "AI整理-中國時報", "AI整理-yahoo財經", "AI整理-工商時報", "AI整理-時報新聞" ].includes(sheetData_name[k])) {
-            const values2_datas = sheetData[k]
-            for (const v of values2_datas.values) {
-                if (v[0] < date_start) {
-                    continue
-                }
-                const news = v[2].replace(/\n\n新聞出處：/,'\n新聞出處：').split("\n\n")
-                for (const v2 of news) {
-                    const v3 = v2.split("\n")
-                    const check = v3.filter(v4=>/^[0-9]+\./.test(v4)).length
-                    if (/：$/.test(v3[0]) && /^新聞出處/.test(v3[v3.length - 1]) && check == v3.length - 2 && v3.length > 2) {
-                        values2_datas_split.push([v[0], v[1], v2])
-                    }
-                }
-            }
-        } else if (sheetData_name[k] == 'news') {
-            // CREATE TABLE `news` (
-            //     `id` BIGINT(19) NOT NULL AUTO_INCREMENT,
-            //     `source` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '新聞來源' COLLATE 'utf8mb4_general_ci',
-            //     `category` VARCHAR(50) NOT NULL DEFAULT '' COMMENT '分類' COLLATE 'utf8mb4_general_ci',
-            //     `create_date` DATE NOT NULL COMMENT '建立日期',
-            //     `create_time` TIME NOT NULL COMMENT '建立時間',
-            //     `url` VARCHAR(700) NOT NULL DEFAULT '' COMMENT '網址' COLLATE 'utf8mb4_general_ci',
-            //     `title` TEXT NOT NULL COMMENT '標題' COLLATE 'utf8mb4_general_ci',
-            //     `short_content` TEXT NOT NULL COMMENT '摘要' COLLATE 'utf8mb4_general_ci',
-            //     `content` LONGTEXT NOT NULL COMMENT '內容' COLLATE 'utf8mb4_general_ci',
-            //     PRIMARY KEY (`id`) USING BTREE,
-            //     UNIQUE INDEX `source_url` (`source`, `url`) USING BTREE,
-            //     INDEX `source` (`source`) USING BTREE,
-            //     INDEX `create_date` (`create_date`) USING BTREE
-            // )
-            // COLLATE='utf8mb4_general_ci'
-            // ENGINE=InnoDB
-            // AUTO_INCREMENT=106
-            // ;
-            
-            const news = sheetData[k]
-            for (const v of news) {
-                if (!values[v.source]) {
-                    values[v.source] = []
-                }
-                values[v.source].push([v.category, `${dayjs(v.create_date).format('YYYY-MM-DD')} ${v.create_time.replace(/:[0-9]$/, '')}`, v.url, v.title, v.short_content, v.content])
-            }
-        } else {
-            values[sheetData_name[k]] = sheetData[k].values
-        }
-    }
-    
-    //排序values2_datas_split
-    values2_datas_split.sort(function (a, b) {
-        return a[0] < b[0] ? 1 : -1
-    })
 
     //------------------------------------------------------------
     //過濾新聞
