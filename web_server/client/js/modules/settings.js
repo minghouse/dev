be3cc0dd4d9d4e2e4fbfc6d07290f2c2822660db
ming.li <ming.li@iwplay.com.tw>
2025-03-28 18:26:20 +0800

    1

--- web_server/client/js/modules/settings.js
+++ web_server/client/js/modules/settings.js
@@ -1,6 +1,6 @@
 const settings = {
-    // api_domain: "https://35.212.208.189",
-    api_domain: "https://localhost:3000",
+    api_domain: "https://35.212.208.189",
+    // api_domain: "https://localhost:3000",
 }
 
 export { settings }
